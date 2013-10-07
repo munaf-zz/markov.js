8f43793d3e5aec4ceea9152138149949bf618017
Munaf Assaf <munaf.assaf@gmail.com>
2013-10-06 20:44:12 -0700

    Working refactor of dictionary object

--- markov.js
+++ markov.js
@@ -1,11 +1,15 @@
 (function() {
 
+  var SENTENCE_START = "##SENTENCE_START##",
+      SENTENCE_END   = "##SENTENCE_END##";
+
   var Markov = function(options) {
 
     this.options = {
-      inputText: options.inputText,
-      order: options.order || 2,
-      numWords: options.numWords || 100
+      inputText: options.inputText,      // The input corpus
+      order: options.order || 2,         
+      numWords: options.numWords || 100, // # words to generate
+      endWithPeriod: true                // end with period even if not end of sentence
     }
     this._setup();
 
@@ -15,7 +19,7 @@
   Markov.prototype = {
 
     // Generate text.
-    generate: function(numWords) {
+    _generate: function(numWords) {
       numWords = numWords || this.params.numWords;
       var outputText = '',
         words, word;
@@ -37,7 +41,7 @@
     },
 
     // Add a corpus to the Markov chain. 
-    addCorpus: function(corpusText) {
+    _addCorpus: function(corpusText) {
       if (!corpusText || corpusText.length === 0) {
         return;
       }
@@ -53,22 +57,102 @@
 
     // Set up the Markov object.
     _setup: function() {
-      this.params = {
-        order: this.options.order
-      };
+      this.dictionary = {};
+      this.dictionary[SENTENCE_START] = [];
+      this.dictionary[SENTENCE_END]   = [SENTENCE_START];
 
-      this._resetPrefix();
-
-      this.params.dictionary = {};
-      this.params.corpus = [];
-      this.params.corpusText = this.options.inputText || '';
-      this.params.latinBased = true;
-
-      if (this.params.corpusText) {
-        this.addCorpus(this.params.corpusText);
-        this._resetDictionary();
-        this._addWord(' ');
+      if (this.options.inputText) {
+        this.addCorpus(this.options.inputText);
       }
+    },
+
+    generate: function(numWords) {
+      console.log(this.dictionary);
+      var generatedText = "",
+          currentWord = SENTENCE_START,
+          followingWord, i = 0;
+
+      while (i < numWords) {
+        followingWord = this._randomFollower(currentWord);
+
+        if (followingWord === SENTENCE_START) {
+        } else if (followingWord === SENTENCE_END) {
+          generatedText += '.';
+        } else if (currentWord === SENTENCE_START) {
+          generatedText += ' ' + followingWord.charAt(0).toUpperCase() + followingWord.slice(1);
+        } else {
+          generatedText += ' ' + followingWord;
+        }
+
+        i++;
+        currentWord = followingWord;
+      }
+
+      if (this.options.endWithPeriod === true) {
+        if (generatedText.charAt(generatedText.length-1) !== '.') {
+          generatedText += '.';
+        }
+      }
+
+      return generatedText;
+    },
+
+    _randomFollower: function(word) {
+      var followingWords = this.dictionary[word];
+      console.log('## ' + word, followingWords);
+      console.log('>> ' + Math.floor(Math.random(followingWords.length)));
+      return followingWords[Math.floor(Math.random()*followingWords.length)];
+    },
+
+    addCorpus: function(text) {
+      text = text
+        .replace(/[-,—?!]/g, "")  // remove punctuation for now
+        .replace(/\s{2,}/g, ' '); // fix double spacing caused by above
+
+      var tokens = text.split(' '); // Tokenize
+      var currentToken, followingToken, terminating;
+
+      for (var i = 0; i < tokens.length; i++) {
+        currentToken = tokens[i]
+          .toLowerCase()
+          .replace(/^\s+|\s+$/g,""); // trim spaces
+
+        if (i !== (tokens.length-1)) {
+          followingToken = tokens[i+1].toLowerCase();
+        }
+
+        // If word ends a sentence, don't include periods in the lookup
+        terminating = false;
+        if (this._isTerminating(currentToken)) {
+          currentToken = this._removePeriod(currentToken);
+          terminating = true;
+        }
+
+        // First time the word is encountered
+        if (!this.dictionary[currentToken]) {
+          this.dictionary[currentToken] = [];
+        }
+
+        // Word ends a sentence
+        if (terminating === true) {
+          this.dictionary[currentToken].push(SENTENCE_END);
+        } else {
+          this.dictionary[currentToken].push(this._removePeriod(followingToken));
+        }
+
+        // Word starts a sentence
+        if (i === 0 || this._isTerminating(tokens[i-1])) {
+          this.dictionary[SENTENCE_START].push(currentToken);
+        }
+      }
+    },
+
+    _removePeriod: function(word) {
+      return word.replace(/\./g, '');
+    },
+
+    _isTerminating: function(word) {
+      return (word.charAt(word.length-1) === '.');
     },
 
     // Start a new Markov Prefix. 
